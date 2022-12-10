8befda14f334edd7468794adec749de7d0bb2a24
hannmx <mxkud@mail.ru>
2022-12-11 00:19:13 +0300

    Ready program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Введите первое граничное значение: ");
+int M = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Введите второе граничное значение: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int num = 1;
+void NaturalNum(int num)
+{
+        if(num % 3 == 0)
+        {
+            Console.Write(num + " ");
+        }
+        num++;
+    if(num > N) 
+    return;
+    NaturalNum(num);
+}
+NaturalNum(num);
